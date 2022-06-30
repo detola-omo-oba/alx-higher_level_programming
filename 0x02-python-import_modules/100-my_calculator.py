21194b9198b8fad0aa8d4f30bd44895a0f34804e
detola-omo-oba <101400387+detola-omo-oba@users.noreply.github.com>
2022-06-30 22:15:53 +0100

    done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-
-
-def my_calc():
-    operations = {"+": add, "-": sub, "*": mul, "/": div}
-    length = len(argv) - 1
-    if (length < 3):
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-
-    a = int(argv[1])
-    b = int(argv[3])
-    operator = argv[2]
-
-    if operator not in operations:
+def calculate(arg, a, b):
+    if arg[2] == "+":
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif arg[2] == "-":
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif arg[2] == "*":
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif arg[2] == "/":
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    result = operations[operator](a, b)
-    print("{} {} {} = {}".format(a, operator, b, result))
 
 
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     from sys import argv
-    my_calc()
+    from calculator_1 import add, sub, mul, div
+
+    if len(argv) is 4:
+        a = int(argv[1])
+        b = int(argv[3])
+
+        calculate(argv, a, b)
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
