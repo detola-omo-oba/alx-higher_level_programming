f90ae5eccc61585a4ebbc6fe166fcb8ef54802df
detola-omo-oba <101400387+detola-omo-oba@users.noreply.github.com>
2022-06-30 22:03:43 +0100

    home

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    import sys
 
-    nargs = len(sys.argv) - 1
-    if nargs != 3:
+def my_calc():
+    operations = {"+": add, "-": sub, "*": mul, "/": div}
+    length = len(argv) - 1
+    if (length < 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    op = sys.argv[2]
-    if op != '+' and op != '-' and op != '*' and op != '/':
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
+
+    if operator not in operations:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
+    result = operations[operator](a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
 
+
+if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-
-    if op == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif op == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif op == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    else:
-        print("{} / {} = {}".format(a, b, div(a, b)))
+    from sys import argv
+    my_calc()
