2a6a9c1b4ed5ceefdcf7bd2508407c7f438c939a
detola-omo-oba <101400387+detola-omo-oba@users.noreply.github.com>
2022-07-01 09:20:30 +0100

    done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-def calculate(arg, a, b):
-    if arg[2] == "+":
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    elif arg[2] == "-":
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    elif arg[2] == "*":
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    elif arg[2] == "/":
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
-
 if __name__ == "__main__":
-    from sys import argv
+    import sys
     from calculator_1 import add, sub, mul, div
-
-    if len(argv) is 4:
-        a = int(argv[1])
-        b = int(argv[3])
-
-        calculate(argv, a, b)
-    else:
+    if (len(sys.argv) != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        op = sys.argv[2]
+        if op == '+':
+            print("{} {} {} = {}".format(a, op, b, add(a, b)))
+        elif op == '-':
+            print("{} {} {} = {}".format(a, op, b, sub(a, b)))
+        elif op == '*':
+            print("{} {} {} = {}".format(a, op, b, mul(a, b)))
+        elif op == '/':
+            print("{} {} {} = {}".format(a, op, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
